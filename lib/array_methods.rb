b2018f3420976a15e39fed6101c890d1e25a4d00
Jahaira Marrero <jahaira.marrero@gmail.com>
2020-12-22 16:34:12 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,6 +1,9 @@
-def using_include(array, element)
+famous_cities = ["New York", "Chicago", "Los Angeles", "Houston"]
 
+def using_include(array, element)
+sorted_cities = famous_cities.sorted
 end
+using_include(array, element)
 
 def using_sort(array)
 
