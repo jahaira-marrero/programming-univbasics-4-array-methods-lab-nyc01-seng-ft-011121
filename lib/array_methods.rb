4ef6f2e206aa62c7d0551d84b5dc4681021c182a
Jahaira Marrero <jahaira.marrero@gmail.com>
2020-12-22 16:49:25 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -4,6 +4,7 @@ def using_include(array, element)
   array.include?(element)
 end
 
+array = ["wow", "I", "am", "really", "learning", "arrays!"]
 def using_sort(array)
  sorted_array = array.sort
  puts sorted_array
