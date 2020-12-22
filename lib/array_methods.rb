c1f2bca30575ba799ca6d3f87a01264d6381c9ab
Jahaira Marrero <jahaira.marrero@gmail.com>
2020-12-22 16:51:57 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -5,13 +5,16 @@ def using_include(array, element)
 end
 
 array = ["wow", "I", "am", "really", "learning", "arrays!"]
+
 def using_sort(array)
  sorted_array = array.sort
  puts sorted_array
 end
 
+array = ["wow", "I", "am", "really", "learning", "arrays!"]
 def using_reverse(array)
-
+  reverse_array = array.reverse
+  puts reverse_array
 end
 
 def using_first(array)
